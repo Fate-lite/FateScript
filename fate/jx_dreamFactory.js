0c2db113d7f93cb32c5b0cfeafb982afe4aaec9f
Fate <fate>
2021-04-07 19:02:56 +0800

    rename

--- fate/jx_dreamFactory.js
+++ fate/jx_dreamFactory.js
@@ -138,10 +138,10 @@ async function jdDreamFactory() {
         await QueryHireReward();// 收取招工电力
         await PickUp(); //收取自家的地下零件
         // await stealFriend();
-        // await tuanActivity();
-        // await QueryAllTuan();
+        await tuanActivity();
+        await QueryAllTuan();
         await exchangeProNotify();
-        // await showMsg();
+        await showMsg();
         // if (helpAu === true) await helpAuthor();
     } catch (e) {
         $.logErr(e)
