4af5591bc290612bf60e76186fd33330b4f82d0a
Fate <fate>
2021-05-21 16:03:56 +0800

    cfd2

--- fate/jx_cfd.js
+++ fate/jx_cfd.js
@@ -94,7 +94,7 @@ const $ = new Env("京喜财富岛");
 const JD_API_HOST = "https://m.jingxi.com/";
 const notify = $.isNode() ? require('./sendNotify') : '';
 const jdCookieNode = $.isNode() ? require("./jdCookie.js") : "";
-const jdTokenNode = $.isNode() ? require('./jdJxncTokens.js') : '';
+// const jdTokenNode = $.isNode() ? require('./jdJxncTokens.js') : '';
 $.showLog = $.getdata("cfd_showLog") ? $.getdata("cfd_showLog") === "true" : false;
 $.notifyTime = $.getdata("cfd_notifyTime");
 $.result = [];
@@ -110,7 +110,7 @@ $.index = 1;
 
 !(async () => {
     if (!getCookies()) return;
-    if (!getTokens()) return;
+    // if (!getTokens()) return;
     for (let i = 0; i < $.cookieArr.length; i++) {
         $.index = i + 1;
         $.currentCookie = $.cookieArr[i];
