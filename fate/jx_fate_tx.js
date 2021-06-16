79aa5b56f3c0c2f54652f06c9de9b1c5f79567e2
Fate <fate>
2021-06-16 09:57:08 +0800

    fix tx3

--- fate/jx_fate_tx.js
+++ fate/jx_fate_tx.js
@@ -22,14 +22,13 @@ $.cookie = process.env.CFDCOOKIE;
 $.userName = '';
 
 !(async () => {
-    if ($.currentCookie) {
+    if ($.cookie) {
         $.nickName = '';
         await TotalBean();
         $.UserName = decodeURIComponent($.cookie.match(/pt_pin=([^; ]+)(?=;?)/) && $.cookie.match(/pt_pin=([^; ]+)(?=;?)/)[1]);
         console.log(`\n*****开始【京东账号${$.index}】${$.nickName || $.UserName}*****\n`);
         if (!$.isLogin) {
             $.msg($.name, `【提示】cookie已失效`, `京东账号${$.index} ${$.nickName || $.UserName}\n请重新登录获取\nhttps://bean.m.jd.com/bean/signIndex.action`, {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
-
             if ($.isNode()) {
                 await notify.sendNotify(`${$.name}cookie已失效 - ${$.UserName}`, `京东账号${$.index} ${$.UserName}\n请重新登录获取cookie`);
             }
