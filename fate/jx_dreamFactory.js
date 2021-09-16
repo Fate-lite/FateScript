426e1f8851c2d98be05d8a7ab5d4aaf275c7c6cf
Fate <fate>
2021-09-16 11:19:39 +0800

    fix2

--- fate/jx_dreamFactory.js
+++ fate/jx_dreamFactory.js
@@ -113,8 +113,6 @@ async function jdDreamFactory() {
         await QueryHireReward();//收取招工电力
         await PickUp();//收取自家的地下零件
         await stealFriend();
-        await tuanActivity();
-        await QueryAllTuan();
         await exchangeProNotify();
         await showMsg();
     } catch (e) {
