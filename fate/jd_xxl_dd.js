0af68f37c19d9c55ae644c9c676dc5fafc9b12cc
Fate <fate>
2021-09-19 17:26:40 +0800

    fix

--- fate/jd_xxl_dd.js
+++ fate/jd_xxl_dd.js
@@ -15,6 +15,7 @@ const notify = $.isNode() ? require('./sendNotify.js') : '';
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 let exchangeName = $.isNode() ? (process.env.EXCHANGE_EC ? process.env.EXCHANGE_EC : '京豆*1888') : ($.getdata('JDEC') ? $.getdata('JDEC') : '京豆*1888')
 
+$.maxLevel = process.env.xxlDDMaxLevel ?? 640;
 let ACT_ID = 'A_112790_R_1_D_20201028'
 //Node.js用户请在jdCookie.js处填写京东ck;
 //IOS等用户直接用NobyDa的jd cookie
