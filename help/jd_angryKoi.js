ac1734149470446bc495612e710e5fbcc6b74226
Fate <fate>
2021-11-30 11:34:20 +0800

    fix by fate 11-30

--- help/jd_angryKoi.js
+++ help/jd_angryKoi.js
@@ -8,7 +8,7 @@ TG学习交流群：https://t.me/cdles
 const $ = new Env("愤怒的锦鲤")
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
 const ua = `jdltapp;iPhone;3.1.0;${Math.ceil(Math.random()*4+10)}.${Math.ceil(Math.random()*4)};${randomString(40)}`
-var kois = process.env.kois ?? ""
+var kois = process.env.kois ?? "24976137-306905"
 let cookiesArr = []
 var helps = [];
 var tools= []
